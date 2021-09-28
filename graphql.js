fb00ecb4418011e3e6c46a8de9e64c6bf696b247
admin <wdev555@gmail.com>
2021-09-28 16:38:16 +0400

    a

---
+++ graphql.js
@@ -0,0 +1,29 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context, express }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+    expressRequest: express.req,
+  }),
+});
+
+exports.graphqlHandler = server.createHandler();
